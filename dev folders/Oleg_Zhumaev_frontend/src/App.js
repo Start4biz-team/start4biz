3eabe2290e1c40351f6f9dd809086f866b05f788
Oleg Zhumaev <ozhu91@yandex.ru>
2020-04-17 21:26:22 +0300

    add block 3

---
+++ dev folders/Oleg_Zhumaev_frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Questions from "./components/Questions.js"
+
+function App() {
+  return (
+    <div className="App">
+      <Questions/>
+    </div>
+  );
+}
+
+export default App;
